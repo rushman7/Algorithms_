fc92f9658f9ec5f0da2a4c5fbfdabb13de26b9a6
BuildTools <arthurpisakhov1@gmail.com>
2019-11-24 21:01:47 -0500

    eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,23 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if not cache:
+    cache = [0 for i in range(n+1)]  # If cache has no value, default cache to an array of zeroes
+
+  if n < 2:
+    return 1
+
+  if n == 2:
+    return 2
+
+  if cache[n] > 2: # If more than 2 ways, return the value of cache
+    return cache[n] # cache[n] is number of ways we can get to n
+  
+  cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+  return cache[n]
+
+eating_cookies(5)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
