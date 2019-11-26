f9c9c32981debf41452203b50e472d6ffc5bf501
BuildTools <arthurpisakhov1@gmail.com>
2019-11-25 20:34:38 -0500

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,32 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass
+options = ['rock', 'paper', 'scissors']
 
+def rock_paper_scissors(n):
+  result = [[] for i in range(3**n)]
+  
+  def game(p):
+    if p == 0:
+      return
+
+    count = 0
+    while count < 3**n:
+      for i in range(3**p//3):
+        result[count].append('rock')
+        count += 1
+      for i in range(3**p//3):
+        result[count].append('paper')
+        count += 1
+      for i in range(3**p//3):
+        result[count].append('scissors')
+        count += 1
+    game(p-1)
+
+  game(n)
+  return result
+
+rock_paper_scissors(2)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
