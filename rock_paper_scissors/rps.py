0090e11d4709aab5595c01c32b30661a9d8c2976
BuildTools <arthurpisakhov1@gmail.com>
2019-11-25 20:02:08 -0500

    eating cookies naive recursive

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,31 +2,8 @@
 
 import sys
 
-options = ['rock', 'paper', 'scissors']
-
 def rock_paper_scissors(n):
-  result = [[] for i in range(3**n)]
-    
-  def game(p):
-    if p == 0:
-      return
-
-    count = 0
-    while count < 3**n:
-      for i in range(3**p//3):
-        result[count].append('rock')
-        count += 1
-      for i in range(3**p//3):
-        result[count].append('paper')
-        count += 1
-      for i in range(3**p//3):
-        result[count].append('scissors')
-        count += 1
-    game(p-1)
-  
-  game(n)
-
-  return result
+  pass
 
 
 if __name__ == "__main__":
